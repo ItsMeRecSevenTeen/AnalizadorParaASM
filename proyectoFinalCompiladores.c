2d0443f12c32867b8849dcc33b024c707c28249b
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-03-03 03:06:46 -0600

    Se agregó un nuevo token para agrupadores, Se agregaron los métodos clasificador de casos, derivador y generador de código intermedio, Esto es prototipo aún

--- proyectoFinalCompiladores.c
+++ proyectoFinalCompiladores.c
@@ -56,7 +56,6 @@ void obtencionContenidoArchivo(){
     printf("\n--------------------------------------\n");
 }
 
-
 void separarPalabrasPorEspacios(){
     char palabra[15] = "";
     int i, j = 0, fila = 0;
@@ -162,7 +161,11 @@ void tokenizador(char cadEnt[100]) {
                                 caso = 2;
                                 tipo = 1;
                             }
-
+                            else if(strchr(OR, let) && lenCad == 1)
+                            {
+                                caso = 5; // relacional
+                                tipo = 8; // asignacion
+                            }
                             else if (strchr(OR, let))
                             {
                                 caso = 5; // relacional
@@ -364,10 +367,10 @@ void tokenizador(char cadEnt[100]) {
                         printf("%-10s = NUMERO ENTERO\n", cadEnt);
                         agregaTablaSimbolos(cadEnt, "NE");
                     }
-                    else if (caso == 4 && tipo == 4)
+                    else if (caso == 5 && tipo == 8)
                     {
-                        printf("%-10s = NUMERO ENTERO\n", cadEnt);
-                        agregaTablaSimbolos(cadEnt, "NE");
+                        printf("%-10s = ASIGNACION\n", cadEnt);
+                        agregaTablaSimbolos(cadEnt, "AS");
                     }
                     else if (caso == 5 && tipo == 2)
                     {
@@ -424,16 +427,16 @@ void tokenizarPalabras(){
         indice++;
     }
 }
-void derivacion(){
-    char produccion1[3][10] = {"SO", "SU", "num"};
-    char produccion2[2][10] = {"*S", "/S"};
-    char produccion3[2][10] = {"+S", "-S"};
+void clasificadorDeCasos(){
     //---------------Clasificacion de casos------------------------
     for (int i = 0; i < k; i++)
     {
         char *end;
         int has_dot = 0;
         int isExp = 0;
+        int is_grouping = 0;
+        int is_relational = 0;
+        int is_asig = 0;
         int start = 0;
 
         // Clasifica el signo del número en caso de que exista
@@ -441,16 +444,31 @@ void derivacion(){
         {
             start = 1;
         }
+        else if (palabras[i][0] == '=') //Clasifica un posible operador relacional '==' o asignación '='
+        {
+            is_asig = 1;//Se asume que es una asignación hasta que se demuestre lo contrario
+            start = 1;
+        }
+        
 
         // Clasifica si es número decimal o no
         int is_number = 1;
         for (int j = start; j < strlen(palabras[i]); j++)
         {
-            if (!isdigit(palabras[i][j]))
-            {
+            if(palabras[i][j] == '<' || palabras[i][j] == '>' || palabras[i][j] == '!' || palabras[i][j] == '&' || palabras[i][j] == '|' || palabras[i][j] == '='){
+                is_relational = 1;
+                is_grouping = 0;
                 is_number = 0;
-                isExp = 0;
                 has_dot = 0;
+                isExp = 0;
+                break;
+
+            }
+            else if(palabras[i][j] == '(' || palabras[i][j] == ')' || palabras[i][j] == '{' || palabras[i][j] == '}' || palabras[i][j] == '[' || palabras[i][j] == ']'){
+                is_grouping = 1;
+                is_number = 0;
+                has_dot = 0;
+                isExp = 0;
                 break;
             }
             else if (palabras[i][j] == '.')
@@ -464,9 +482,53 @@ void derivacion(){
                 is_number = 1;
                 j++;
             }
+            else if (!isdigit(palabras[i][j]))
+            {
+                is_number = 0;
+                isExp = 0;
+                has_dot = 0;
+                break;
+            }
+            
         }
         if ((strcmp(palabras[i], "Var") == 0 || strcmp(palabras[i], "var") == 0) && is_number == 0 && has_dot == 0 && isExp == 0){
             strcpy(lexemas[i], "var");
+        }
+        else if(strcmp(palabras[i],"=") == 0 && is_asig == 1){
+            strcpy(lexemas[i], "=");
+        }
+        else if(strcmp(palabras[i],"int") == 0){
+            strcpy(lexemas[i], "int");
+        }
+        else if(strcmp(palabras[i],"float") == 0){
+            strcpy(lexemas[i], "float");
+        }
+        else if(strcmp(palabras[i],"char") == 0){
+            strcpy(lexemas[i], "char");
+        }
+        else if(strcmp(palabras[i],"if") == 0){
+            strcpy(lexemas[i], "if");
+        }
+        else if(strcmp(palabras[i],"else") == 0){
+            strcpy(lexemas[i], "else");
+        }
+        else if(strcmp(palabras[i],"for") == 0){
+            strcpy(lexemas[i], "for");
+        }
+        else if(strcmp(palabras[i],"while") == 0){
+            strcpy(lexemas[i], "while");
+        }
+        else if(strcmp(palabras[i],"switch") == 0){
+            strcpy(lexemas[i], "switch");
+        }
+        else if(strcmp(palabras[i],"case") == 0){
+            strcpy(lexemas[i], "case");
+        }
+        else if(strcmp(palabras[i],"default") == 0){
+            strcpy(lexemas[i], "default");
+        }
+        else if(strcmp(palabras[i],"return") == 0){
+            strcpy(lexemas[i], "return");
         }
         else if(is_number && strlen(palabras[i]) > start)
         {
@@ -480,7 +542,13 @@ void derivacion(){
         {
             strcpy(lexemas[i], "U");
         }
-        else if(is_number == 0 && isExp == 0 && has_dot == 0){
+        else if(is_relational){
+            strcpy(lexemas[i], "OR");
+        }
+        else if(is_grouping){
+            strcpy(lexemas[i], "AG");
+        }
+        else if((is_number == 0 && isExp == 0 && has_dot == 0)!= 0 && is_grouping == 0 && is_relational == 0){
             strcpy(lexemas[i], "id");
         }
         else if (is_number && has_dot && isExp)
@@ -489,12 +557,263 @@ void derivacion(){
         }
     }
 }
+//Producciones
+//PARA OPERACIONES ARITMÉTICAS:
+// S -> SO | SU | num
+// O -> +S | -S
+// U -> *S | /S
+
+//PARA LA ESTRUCTURA FOR:
+//for( I ; C ; A ){sentencias}
+// A -> id++ | id-- | ++id | --id
+// I -> id = num | id = id
+// C -> id OR num | id OR id
+// OR -> < | > | <= | >= | == | !=
+void derivador() {
+    printf("\n------------Derivacion por LL(1)--------------:\n");
+    
+    int index = 0;
+    int linea = 1;
+
+    // Procesando todos los lexemas
+    while (index < k) {
+        char derivacion[200] = "S";
+        printf("\nEstructura %d:\n", linea++);
+        printf("%s\n", derivacion);
+        
+        int terminado = 0;
+
+        while (!terminado && index < k) {
+            //Descarte de AG
+            while (index < k && strcmp(lexemas[index], "AG") == 0) {
+                index++;
+            }
+            if (index >= k) break;
+
+            // Búsqueda del No-Terminal más a la izquierda
+            // S=Statement, T=Tipo, I=Init, C=Condicion, A=Aumento, R=Relacional, E=Expresion, O/U=Operadores
+            char *pos = strpbrk(derivacion, "STICAEROU");
+            
+            if (pos == NULL) {
+                terminado = 1;
+                continue; // Saltarse la línea actual
+            }
+
+            char nueva[200] = "";
+            strncat(nueva, derivacion, pos - derivacion); // Copia lo que hay antes del No-Terminal
+
+            // S: Sentencia inicial
+            if (*pos == 'S') {
+                if (strcmp(lexemas[index], "var") == 0) {
+                    strcat(nueva, "var T id = E");
+                    index++; // var consumido
+                }
+                else if (strcmp(lexemas[index], "for") == 0) {
+                    strcat(nueva, "for( I ; C ; A ){ S }");
+                    index++; // for consumido
+                }
+                else if (strcmp(lexemas[index], "if") == 0) {
+                    strcat(nueva, "if( C ){ S }");
+                    index++; // if consumido
+                }
+                //else if (strcmp(lexemas[index], "int") == 0 || strcmp(lexemas[index], "float") == 0 || strcmp(lexemas[index], "char") == 0) {
+                //    strcat(nueva, "T id = E"); // Declaración
+                //}
+                else if (strcmp(lexemas[index], "id") == 0 && index + 1 < k && strcmp(lexemas[index+1], "=") == 0) {
+                    strcat(nueva, "id = E"); // Asignación
+                    index += 2; // id y = consumido
+                }
+                else if (strcmp(lexemas[index], "return") == 0) {
+                    strcat(nueva, "return E");
+                    index++;
+                }
+                else {
+                    strcat(nueva, "E"); // Expresión si no evaluó condiciones
+                }
+            }
+            //T: Tipo dato
+            else if (*pos == 'T') {
+                strcat(nueva, palabras[index]); // int, float, char...
+                index++;
+            }
+            // 3. for ( I , C , A )
+            else if (*pos == 'I') {
+                strcat(nueva, "id = E");
+                index += 2; // id = E
+            }
+            else if (*pos == 'C') {
+                strcat(nueva, "E R E"); // Expresión OperadorRelacional Expresión
+            }
+            else if (*pos == 'A') {
+                strcat(nueva, "id");
+                index++; // id consumido
+                // Caso para ++ y --, se toman como caracter unitario (+,-)
+                while (index < k && (strcmp(palabras[index], "+") == 0 || strcmp(palabras[index], "-") == 0)) {
+                    strcat(nueva, palabras[index]);
+                    index++;
+                }
+            }
+            // 4. R: Operadores
+            else if (*pos == 'R') {
+                strcat(nueva, palabras[index]); // <, >, ==.
+                index++;
+            }
+            // E: Expresiones
+            else if (*pos == 'E') {
+
+                int sig_O = 0, sig_U = 0;
+                // Si entra a E, Significa que es una expresión, por lo tanto contiene SO o SU
+                if (index + 1 < k) {
+                    if (strcmp(lexemas[index + 1], "=") == 0){
+                        index += 2; // Consume el '=' y el siguiente token
+                        if (strcmp(lexemas[index], "O") == 0) sig_O = 1;
+                        if (strcmp(lexemas[index], "U") == 0) sig_U = 1;
+                    }
+                    else if (strcmp(lexemas[index + 1], "O") == 0) sig_O = 1;
+                    else if (strcmp(lexemas[index + 1], "U") == 0) sig_U = 1;
+                }
+
+                if (strncmp(pos, "EO", 2) == 0 || strncmp(pos, "EU", 2) == 0) {
+                    strcat(nueva, lexemas[index]); // Transforma en id o num
+                    index++; //Consume id o num
+                } 
+                else {
+                    if (sig_O) strcat(nueva, "EO");
+                    else if (sig_U) strcat(nueva, "EU");
+                    else {
+                        strcat(nueva, lexemas[index]); // Fin de expresión
+                        index++;
+                    }
+                }
+            }
+            // 6. O y Y: Operadores aritmeticos
+            else if (*pos == 'O') {
+                if (strcmp(palabras[index], "+") == 0) strcat(nueva, "+E");
+                else strcat(nueva, "-E");
+                index++;
+            }
+            else if (*pos == 'U') {
+                if (strcmp(palabras[index], "*") == 0) strcat(nueva, "*E");
+                else strcat(nueva, "/E");
+                index++;
+            }
+
+            // Copiamos el resto de la cadena
+            strcat(nueva, pos + 1);
+            strcpy(derivacion, nueva);
+            printf("%s\n", derivacion);
+        }
+    }
+}
+
+// Variables globales para TAC
+int cont_temp = 1;
+int cont_etiqueta = 1;
+
+void evaluarExpresionCompleja(int inicio, int fin, char* destino) {
+    char exp[50][30];
+    int len = 0;
+
+    // PASO 1: Copiar la expresión y pedir variables desconocidas
+    for (int i = inicio; i <= fin; i++) {
+        if (strcmp(lexemas[i], "id") == 0) {
+            char valorIngresado[30];
+            // Aquí puedes buscar en tu tabla de símbolos el tipo de dato real
+            // Por ahora, simulamos la petición al usuario
+            printf(">> Ingrese el valor para la variable '%s': ", palabras[i]);
+            scanf("%s", valorIngresado);
+            
+            // Reemplazamos el 'id' directamente por el número ingresado
+            strcpy(exp[len], valorIngresado); 
+        } else {
+            strcpy(exp[len], palabras[i]);
+        }
+        len++;
+    }
+
+    // PASO 2: Resolver Multiplicaciones y Divisiones (Prioridad Alta)
+    for (int i = 0; i < len; i++) {
+        if (strcmp(exp[i], "*") == 0 || strcmp(exp[i], "/") == 0) {
+            // Generamos TAC
+            printf("t%d = %s %s %s\n", cont_temp, exp[i-1], exp[i], exp[i+1]);
+            
+            // El resultado ocupa el lugar del primer operando
+            sprintf(exp[i-1], "t%d", cont_temp++);
+            
+            // Recorremos el arreglo 2 posiciones a la izquierda para borrar el operador y el segundo operando
+            for (int j = i; j < len - 2; j++) {
+                strcpy(exp[j], exp[j+2]);
+            }
+            len -= 2;
+            i--; // Retrocedemos el índice para no saltarnos operadores adyacentes
+        }
+    }
+
+    // PASO 3: Resolver Sumas y Restas (Prioridad Baja)
+    for (int i = 0; i < len; i++) {
+        if (strcmp(exp[i], "+") == 0 || strcmp(exp[i], "-") == 0) {
+            // Generamos TAC
+            printf("t%d = %s %s %s\n", cont_temp, exp[i-1], exp[i], exp[i+1]);
+            
+            sprintf(exp[i-1], "t%d", cont_temp++);
+            
+            for (int j = i; j < len - 2; j++) {
+                strcpy(exp[j], exp[j+2]);
+            }
+            len -= 2;
+            i--; 
+        }
+    }
+
+    // PASO 4: Asignación final
+    printf("%s = %s\n", destino, exp[0]);
+}
+
+void generarCodigoIntermedio() {
+    printf("\n------------ Codigo de 3 Direcciones (TAC) --------------\n");
+    int i = 0;
+    
+    while (i < k) {
+        // Ignoramos tipos de datos al inicio de la línea (int, float, etc.)
+        if (strcmp(lexemas[i], "int") == 0 || strcmp(lexemas[i], "float") == 0) {
+            i++;
+        }
+
+        // Detectar asignación: id = Expresion
+        if (strcmp(lexemas[i], "id") == 0 && i + 1 < k && strcmp(lexemas[i+1], "=") == 0) {
+            char destino[30];
+            strcpy(destino, palabras[i]);
+            
+            int inicio_expr = i + 2;
+            int fin_expr = inicio_expr;
+            
+            // Buscar dónde termina la expresión (hasta encontrar un punto y coma, otra estructura o el fin del arreglo)
+            while (fin_expr < k && strcmp(palabras[fin_expr], ";") != 0 && strcmp(lexemas[fin_expr], "if") != 0) {
+                fin_expr++;
+            }
+            fin_expr--; // Retrocedemos uno para no incluir el ';' o la siguiente palabra
+            
+            // Llamamos a nuestra función destructora de expresiones
+            evaluarExpresionCompleja(inicio_expr, fin_expr, destino);
+            
+            i = fin_expr + 1; // Avanzamos el cursor principal
+            if (i < k && strcmp(palabras[i], ";") == 0) i++; // Saltamos el punto y coma si existe
+        }
+        else {
+            i++; // Si no es asignación, avanzamos (aquí meteremos el IF y el FOR luego)
+        }
+    }
+    printf("\n---------------------------------------------------------\n");
+}
+
 int main(){
     obtenerRutaArchivo();
     obtencionContenidoArchivo();
     separarPalabrasPorEspacios();
     tokenizarPalabras();
     mostrarTablaDeSimbolos();
-    derivacion();
+    clasificadorDeCasos();
+    derivador();
+    generarCodigoIntermedio();
     return 0;
 }
